66523c70990e2cdb2d7b6c99e99c424bf8af88ea
zezerinodev <mattoszz@hotmail.com>
2023-02-01 16:52:07 -0300

    Section Portfólio de home finalizada

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -178,7 +178,7 @@ export default function Home(){
                     </div>
                 </div>
                 
-                <Link to=''>CONHEÇA MAIS PROJETOS</Link>
+                <Link to='/portfolio' className='animation'>CONHEÇA MAIS PROJETOS</Link>                
             </section>
         </>
         
