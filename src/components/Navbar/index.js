3ddf6b265c3e2aa36447730e6789d912f9e9f654
zezerinodev <mattoszz@hotmail.com>
2023-02-10 16:28:39 -0300

    Ajuste nav link Curriculo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -33,7 +33,7 @@ export default function Navbar(){
                 <li><Links activeClass="active" to="sobre" spy={true} smooth={true} offset={-110} duration={1000} className={option === 'sobre' ? 'optionActived' : null} onClick={() => setOption('sobre')} >Quem Sou</Links></li>
                 <li><Links activeClass="active" to="sills" spy={true} smooth={true} offset={0} duration={1000} className={option === 'skills' ? 'optionActived' : null} onClick={() => setOption('skills')} >Skills</Links></li>
                 <li><a href={urlYouTube} target='blank' className={option === 'youtube' ? 'optionActived' : null} onClick={() => setOption('youtube')}>Canal YouTube</a></li>
-                <li><Link to='curriculo' target='blank' className={option === 'curriculo' ? 'optionActived' : null} onClick={() => setOption('curriculo')}>Curriculo</Link></li>               
+                <li><Link to='curriculo' className={option === 'curriculo' ? 'optionActived' : null} onClick={() => setOption('curriculo')}>Curriculo</Link></li>               
                 <li><Link to='contato' className={option === 'contato' ? 'optionActived' : null} onClick={() => setOption('contato')} >Contato</Link></li>
 
                 {actived && <li><Link to='/portfolio' className={option === 'portfolio' ? 'optionActived' : null} onClick={() => setOption('portfolio')} >Conheça meu portfólio</Link></li> }
